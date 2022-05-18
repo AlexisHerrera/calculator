c2f3976d14132f759162b6d0f108cb50bcdcc39c
Alexis Herrera <alexysaguilar@gmail.com>
2022-05-18 17:15:11 -0300

    Adds operations

--- script.js
+++ script.js
@@ -35,9 +35,63 @@ function connectButtonsToDisplay() {
         )
 }
 
+function getOperator(operatorName) {
+    switch (operatorName) {
+        case 'add':
+            return add;
+        case 'substract':
+            return substract;
+        case 'multiply':
+            return multiply;
+        case 'divide':
+            return divide;
+        default:
+            return null;
+      }
+}
+// // First get operator
+// let operator = getOperator(operatorName);
+// if (!operator) return null;
+
+function startOperation(operatorName) {
+    // Save base operand
+    const displayBoard = document.getElementById('display');
+    displayBoard.dataset.leftOperand = getNumberOfDisplay();
+    // Save operator
+    displayBoard.dataset.operator = operatorName;
+    // Clear -- FOR NOW -- 
+    displayBoard.innerText = "";
+}
+
+function connectOperatorsToDisplay() {
+    const operatorsButtons = Array.from(document.getElementById('operators').getElementsByTagName('button'));
+    operatorsButtons.forEach(button =>
+        button.addEventListener('click',() => startOperation(button.id))
+         );
+}
+
+
 function getNumberOfDisplay() {
     const displayBoard = document.getElementById('display');
     return Number(displayBoard.innerText);
 }
 
+function completeOperation() {
+    const displayBoard = document.getElementById('display');
+    // Get values
+    let leftOperand = Number(displayBoard.dataset.leftOperand);
+    let operator = getOperator(displayBoard.dataset.operator);
+    let rightOperand = getNumberOfDisplay();
+    // Calculate and display
+    let result = operate(operator, leftOperand, rightOperand);
+    displayBoard.innerText = result;
+}
+
+function connectEqualsToDisplay() {
+    const equalsButton = document.getElementById('equals');
+    equalsButton.addEventListener('click', completeOperation);
+}
+
 connectButtonsToDisplay();
+connectOperatorsToDisplay();
+connectEqualsToDisplay();
