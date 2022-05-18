e6cb7da7db78959618ce8fee5471d8779b2a57c5
Alexis Herrera <alexysaguilar@gmail.com>
2022-05-18 15:23:00 -0300

    Connect numbers to display. Get number of display

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ function divide(x, y) {
 function operate(operator, x, y) {
     return operator(x, y);
 }
+
+/* Wire buttons to display */
+function placeNumberInDisplay(content) {
+    const displayBoard = document.getElementById('display');
+    displayBoard.innerText += content;
+}
+// Accesors
+// const displayBoard = document.getElementById('display');
+// const numberButtons = document.querySelectorAll('[data-number]');
+
+function connectButtonsToDisplay() {
+    const numberButtons = document.querySelectorAll('[data-number]');
+    numberButtons.forEach(button =>
+        button.addEventListener('click', () => placeNumberInDisplay(button.dataset.number))
+        )
+}
+
+function getNumberOfDisplay() {
+    const displayBoard = document.getElementById('display');
+    return Number(displayBoard.innerText);
+}
+
+connectButtonsToDisplay();
