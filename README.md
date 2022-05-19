3d76a8be15e84ba72d10d47f0c05135c7efea9cf
Alexis Herrera <alexysaguilar@gmail.com>
2022-05-19 15:41:21 -0300

    Scenarios 1, 2: completed

--- README.md
+++ README.md
@@ -5,10 +5,20 @@ https://www.theodinproject.com/lessons/foundations-calculator
 ## Features
 
 ```gherkin
-Scenario: String operations
-    Given exists a started operation (7 *)
+Scenario: 1 - Basic display
+    When exists an started operation (7 *)
+    Then it should display 7
+
+Scenario: 2 - Display clears when entering a value after operator
+    When exists an started operation (7 *)
+    And I input another value (5)
+    Then it should display 5
+
+Scenario: 3 - String operations
+    When exists an started operation (7 *)
     And I input another value (5)
     When I press a button to make a new operation (+)
     Then the previous operation is finished and is shown in display (7 * 5 = 35)
-    And exists a started operation (35 +)
+    And exists an started operation (7 *)
+    
 ```
--- script.js
+++ script.js
@@ -19,19 +19,101 @@ function operate(operator, x, y) {
     return operator(x, y);
 }
 
-/* Wire buttons to display */
-function placeNumberInDisplay(content) {
+/* Getters and setters of HTML*/
+function getNumberOfDisplay() {
     const displayBoard = document.getElementById('display');
-    displayBoard.innerText += content;
+    return Number(displayBoard.innerText);
 }
 
-function connectButtonsToDisplay() {
+function setInDisplay(content) {
+    const displayBoard = document.getElementById('display');
+    displayBoard.innerText = content;
+    // This is ugly..
+    displayBoard.dataset.result = "";
+}
+
+function setInDisplayAsResult(content) {
+    setInDisplay(content);
+    const displayBoard = document.getElementById('display');
+    displayBoard.dataset.result = content;
+}
+
+/* Checkers */
+function isDisplayAResult() {
+    const displayBoard = document.getElementById('display');
+    let result = displayBoard.dataset.result;
+    return result !== undefined && result !== "";
+}
+
+function isCalculatorClear() {
+    const displayBoard = document.getElementById('display');
+    return displayBoard.innerText == "0";
+}
+
+function existsAnStartedCalculation() {
+    const leftOperandElement = document.getElementById('left-operand');
+    const operatorElement = document.getElementById('operator');
+    return leftOperandElement.innerText !== "" && operatorElement.innerText !== "";
+}
+
+/* Wire buttons to display */
+function connectNumberButtonsToDisplay() {
     const numberButtons = document.querySelectorAll('[data-number]');
     numberButtons.forEach(button =>
         button.addEventListener('click', () => placeNumberInDisplay(button.dataset.number))
         )
 }
 
+function connectOperatorsToDisplay() {
+    const operatorsButtons = Array.from(document.getElementById('operators').getElementsByTagName('button'));
+    operatorsButtons.forEach(button =>
+        button.addEventListener('click',() => executeOperation(button.id))
+         );
+}
+
+function connectEqualsToDisplay() {
+    const equalsButton = document.getElementById('equals');
+    equalsButton.addEventListener('click', completeOperation);
+}
+
+/* Events triggered by buttons */
+function placeNumberInDisplay(content) {
+    if (isDisplayAResult() || isCalculatorClear()) {
+        setInDisplay(content);
+    } else {
+        setInDisplay(`${getNumberOfDisplay()}${content}`);
+    }
+
+}
+
+function executeOperation(operatorName) {
+    // Save left operand
+    const leftOperandElement = document.getElementById('left-operand');
+    leftOperandElement.innerText = getNumberOfDisplay();
+    // Save operator
+    const operatorElement = document.getElementById('operator');
+    operatorElement.innerText = operatorName;
+    // Display left operand and set it to "result"
+    setInDisplayAsResult(getNumberOfDisplay());
+}
+
+
+// redefine equals (debe limpiar left operand y operator)
+// el +,*,/,- debe encadenar (lo mismo que ya hace...)
+
+function completeOperation() {
+    const leftOperandElement = document.getElementById('left-operand');
+    const operatorElement = document.getElementById('operator');
+    // Get values
+    let leftOperand = Number(leftOperandElement.innerText);
+    let operator = getOperator(operatorElement.innerText);
+    let rightOperand = getNumberOfDisplay();
+    // Calculate and display. Set it as result
+    let result = operate(operator, leftOperand, rightOperand);
+    setInDisplay(result);
+}
+
+/* Parser */
 function getOperator(operatorName) {
     switch (operatorName) {
         case 'add':
@@ -46,64 +128,11 @@ function getOperator(operatorName) {
             return null;
       }
 }
+/* App */
 
-function existsAnStartedCalculation() {
-    const leftOperandElement = document.getElementById('left-operand');
-    const operatorElement = document.getElementById('operator');
-    return leftOperandElement.innerText !== "" && operatorElement.innerText !== "";
-}
-
-function executeOperation(operatorName) {
-    if (existsAnStartedCalculation()) {
-        completeOperation();
-    }
-
-    // Save base operand
-    const leftOperandElement = document.getElementById('left-operand');
-    leftOperandElement.innerText = getNumberOfDisplay();
-    // Save operator
-    const operatorElement = document.getElementById('operator');
-    operatorElement.innerText = operatorName;
-    // Clear -- FOR NOW --
-    const displayBoard = document.getElementById('display');
-    displayBoard.innerText = "";
-}
-
-function connectOperatorsToDisplay() {
-    const operatorsButtons = Array.from(document.getElementById('operators').getElementsByTagName('button'));
-    operatorsButtons.forEach(button =>
-        button.addEventListener('click',() => executeOperation(button.id))
-         );
-}
-
-
-function getNumberOfDisplay() {
-    const displayBoard = document.getElementById('display');
-    return Number(displayBoard.innerText);
-}
-
-function completeOperation() {
-    const displayBoard = document.getElementById('display');
-    const leftOperandElement = document.getElementById('left-operand');
-    const operatorElement = document.getElementById('operator');
-    // Get values
-    let leftOperand = Number(leftOperandElement.innerText);
-    let operator = getOperator(operatorElement.innerText);
-    let rightOperand = getNumberOfDisplay();
-    // Calculate and display
-    let result = operate(operator, leftOperand, rightOperand);
-    displayBoard.innerText = result;
-}
-
-function connectEqualsToDisplay() {
-    const equalsButton = document.getElementById('equals');
-    equalsButton.addEventListener('click', completeOperation);
-}
-
-connectButtonsToDisplay();
+connectNumberButtonsToDisplay();
 connectOperatorsToDisplay();
 connectEqualsToDisplay();
-
 // Accesors
 // const displayBoard = document.getElementById('display');
 // const numberButtons = document.querySelectorAll('[data-number]');
